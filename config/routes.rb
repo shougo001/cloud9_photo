1faf8f98c37ad780b3cd293be108b9b9b19b300b
shougo001 <goosehouse4040@gmail.com>
2020-02-03 05:47:02 +0000

    投稿機能の実装、投稿ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get '/users/:id', to: 'users#show', as: 'user'
+  
+  resources :posts, only: %i(new create index) do
+    resources :photos, only: %i(create)
+  end
 end
